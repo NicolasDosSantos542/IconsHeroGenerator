83d18c3ef5dbc8ed0c7b070b6e1408532a6fcf5a
dxmprofuse <administrateur@dxmprofuse.fr>
2026-03-17 13:34:16 +0100

    Replace data.js with data-great_powers.js for power attributes and remove obsolete code; update index.html to reference the new data file.

--- js/data-great_powers.js
+++ js/data-great_powers.js
@@ -39,173 +39,182 @@ let table = {
   ],
   alteration: [
     {
-      name: "Augmentation de Capacité",
+      name: "Augmentation de Capacité (groupe)",
       first: [1, 2],
       second: [1],
+      page: 34,
     },
     {
-      name: "Renforcement de Capacité",
+      name: "Renforcement de Capacité (groupe)",
       first: [1, 2],
       second: [2],
+      page: 99,
     },
     {
       name: "Alter-ego",
       first: [1, 2],
       second: [3],
+      page: 32,
     },
     {
-      name: "Forme alternative",
+      name: "Forme alternative (groupe)",
       first: [1, 2],
       second: [4],
+      page: 72,
     },
     {
       name: "Amphibie",
       first: [1, 2],
       second: [5],
+      page: 32,
     },
     {
       name: "Densité",
       first: [1, 2],
       second: [6],
+      page: 64,
+    },
+
+    {
+      name: "Contrôle dimensionnel",
+      first: [3, 4],
+      second: [1],
+      page: 56,
     },
     {
       name: "Duplication",
-
-      first: [3, 4],
-      second: [1],
-    },
-    {
-      name: "Membres additionnels",
-
       first: [3, 4],
       second: [2],
+      page: 68,
+    },
+    {
+      name: "Évolution",
+      first: [3, 4],
+      second: [3],
+      page: 72,
+    },
+    {
+      name: "Membres additionnels (groupe)",
+      first: [3, 4],
+      second: [4],
+      page: 84,
+    },
+    {
+      name: "Gestalt",
+      first: [3, 4],
+      second: [5],
+      page: 76,
     },
     {
       name: "Gigantisme",
-
       first: [3, 4],
-      second: [3],
+      second: [6],
+      page: 77,
     },
+
     {
       name: "Invisibilité",
-
-      first: [3, 4],
-      second: [4],
+      first: [5, 6],
+      second: [1],
+      page: 83,
+    },
+    {
+      name: "Mimétisme (groupe)",
+      first: [5, 6],
+      second: [2],
+      page: 87,
     },
     {
       name: "Immatérialité",
-
-      first: [3, 4],
-      second: [5],
+      first: [5, 6],
+      second: [3],
+      page: 80,
     },
     {
       name: "Diminution",
-
-      first: [3, 4],
-      second: [6],
-    },
-    {
-      name: "Mimétisme animal",
-
-      first: [5, 6],
-      second: [1],
-    },
-    {
-      name: "Mimétisme matériel",
-
-      first: [5, 6],
-      second: [2],
-    },
-    {
-      name: "Mimétisme végétal",
-
-      first: [5, 6],
-      second: [3],
-    },
-    {
-      name: "Mimétisme de pouvoir",
-
       first: [5, 6],
       second: [4],
+      page: 65,
     },
     {
       name: "Élasticité",
-
       first: [5, 6],
       second: [5],
+      page: 70,
     },
     {
       name: "Métamorphose",
-
       first: [5, 6],
       second: [6],
+      page: 85,
     },
   ],
   control: [
     {
-      name: "Rayon altérant",
+      name: "Rayon altérant (groupe)",
       first: [1, 2],
       second: [1, 2],
+      page: 98,
     },
     {
-      name: "Contrôle élémentaire",
+      name: "Contrôle du Continuum (groupe)",
       first: [1, 2],
       second: [3, 4],
+      page: 57,
     },
     {
-      name: "Contrôle des probabilités",
+      name: "Contrôle élémentaire (groupe)",
       first: [1, 2],
-      second: [5],
+      second: [5, 6],
+      page: 59,
     },
     {
-      name: "Contrôle du temps",
-      first: [1, 2],
-      second: [6],
-    },
-    {
-      name: "Contrôle de l’énergie",
+      name: "Contrôle de l’énergie (groupe)",
       first: [3, 4],
       second: [1, 2],
+      page: 46,
     },
     {
       name: "Guérison",
       first: [3, 4],
       second: [3],
+      page: 78,
     },
     {
-      name: "Télékinésie",
+      name: "Contrôle des machines",
       first: [3, 4],
-      second: [4, 5],
-    },
-    {
-      name: "Transmutation",
-      first: [3, 4],
-      second: [6],
-    },
-    {
-      name: "Pouvoir cosmique",
-      first: [5, 6],
-      second: [1],
-    },
-    {
-      name: "Gadgets",
-      first: [5, 6],
-      second: [2, 3],
-    },
-    {
-      name: "Sorcellerie",
-      first: [5, 6],
       second: [4],
+      page: 50,
     },
     {
-      name: "Nullification",
+      name: "Contrôle de la matière (groupe)",
+      first: [3, 4],
+      second: [5, 6],
+      page: 43,
+    },
+    {
+      name: "Contrôle de pouvoir (groupe)",
       first: [5, 6],
-      second: [5],
+      second: [1, 2],
+      page: 47,
     },
     {
       name: "Serviteur",
       first: [5, 6],
-      second: [6],
+      second: [3],
+      page: 101,
+    },
+    {
+      name: "Contrôle des esprits",
+      first: [5, 6],
+      second: [4],
+      page: 49,
+    },
+    {
+      name: "Arcanes (groupe)",
+      first: [5, 6],
+      second: [5, 6],
+      page: 33,
     },
   ],
   defense: [
@@ -213,41 +222,49 @@ let table = {
       name: "Absorption",
       first: [1, 2],
       second: [1, 2],
+      page: 29,
     },
     {
       name: "Adaptation",
       first: [1, 2],
       second: [3],
+      page: 30,
     },
     {
-      name: "Champ de Force",
+      name: "Champ de force",
       first: [1, 2],
       second: [4, 5, 6],
+      page: 38,
     },
     {
       name: "Immortalité",
       first: [3, 4],
       second: [1],
+      page: 81,
     },
     {
       name: "Vitalité",
       first: [3, 4],
       second: [2, 3, 4],
+      page: 110,
     },
     {
       name: "Miroir",
       first: [3, 4],
       second: [5, 6],
+      page: 90,
     },
     {
       name: "Régénération",
       first: [5, 6],
       second: [1, 2],
+      page: 99,
     },
     {
-      name: "Résistance",
+      name: "Résistance (groupe)",
       first: [5, 6],
-      second: [3, , 4, 5, 6],
+      second: [3, 4, 5, 6],
+      page: 100,
     },
   ],
   spirit: [
@@ -255,101 +272,105 @@ let table = {
       name: "Projection astrale",
       first: [1, 2, 3],
       second: [1],
+      page: 97,
     },
     {
       name: "Contrôle des rêves",
       first: [1, 2, 3],
       second: [2],
+      page: 54,
     },
     {
       name: "Contrôle des émotions",
       first: [1, 2, 3],
       second: [3, 4],
+      page: 48,
     },
     {
-      name: "Illusion",
+      name: "Illusion ou Images",
       first: [1, 2, 3],
-      second: [5],
-    },
-    {
-      name: "Images",
-      first: [1, 2, 3],
-      second: [6],
+      second: [5, 6],
+      page: 79,
     },
     {
       name: "Décharge mentale",
       first: [4, 5, 6],
       second: [1, 2],
+      page: 63,
     },
     {
       name: "Domination",
       first: [4, 5, 6],
       second: [3],
+      page: 66,
     },
     {
       name: "Bouclier mental",
       first: [4, 5, 6],
       second: [4],
+      page: 38,
     },
     {
       name: "Télépathie",
       first: [4, 5, 6],
       second: [5, 6],
+      page: 106,
     },
   ],
   movement: [
     {
       name: "Fouissage",
-
       first: [1, 2, 3],
       second: [1],
+      page: 74,
     },
     {
       name: "Voyage dimensionnel",
-
       first: [1, 2, 3],
       second: [2],
+      page: 112,
     },
     {
       name: "Vol",
-
       first: [1, 2, 3],
       second: [3, 4],
+      page: 111,
     },
     {
       name: "Bonds",
-
       first: [1, 2, 3],
       second: [5, 6],
+      page: 37,
     },
     {
       name: "Toupie",
-
       first: [4, 5, 6],
       second: [1],
+      page: 108,
     },
     {
       name: "Super-vitesse",
-
       first: [4, 5, 6],
       second: [2, 3],
+      page: 105,
     },
     {
       name: "Voltige",
-
       first: [4, 5, 6],
       second: [4],
+      page: 111,
     },
     {
       name: "Téléportation",
-
       first: [4, 5, 6],
       second: [5],
+      page: 107,
     },
     {
       name: "Accroches",
       first: [4, 5, 6],
       second: [6],
+      page: 29,
     },
   ],
   attack: [
@@ -357,83 +378,111 @@ let table = {
       name: "Affliction",
       first: [1, 2, 3],
       second: [1],
+      page: 30,
+    },
+    {
+      name: "Aura",
+      first: [1, 2, 3],
+      second: [2],
+      page: 36,
     },
     {
       name: "Immobilisation",
       first: [1, 2, 3],
-      second: [2],
+      second: [3],
+      page: 81,
     },
     {
       name: "Décharge",
       first: [1, 2, 3],
-      second: [3, 4],
+      second: [4,5],
+      page: 63,
     },
     {
-      name: "Frappe",
+      name: "Corrosion",
       first: [1, 2, 3],
-      second: [5, 6],
-    },
-    {
-      name: "Aura",
-      first: [4, 5, 6],
-      second: [1],
+      second: [6],
+      page: 62,
     },
     {
       name: "Aveuglement",
       first: [4, 5, 6],
-      second: [2, 3],
+      second: [1],
+      page: 37,
     },
     {
-      name: "Drain de vie",
+      name: "Drain d’énergie",
       first: [4, 5, 6],
-      second: [4],
+      second: [2],
+      page: 68,
     },
     {
       name: "Attaque rapide",
       first: [4, 5, 6],
-      second: [5],
+      second: [3],
+      page: 34,
+    },
+    {
+      name: "Frappe",
+      first: [4, 5, 6],
+      second: [4,5],
+      page: 75,
     },
     {
       name: "Paralysie",
       first: [4, 5, 6],
       second: [6],
+      page: 93,
     },
   ],
   perception: [
     {
-      name: "Détection",
+      name: "Sens du danger",
       first: [1, 2, 3],
       second: [1, 2],
+      page: 100,
+    },
+    {
+      name: "Détection (groupe)",
+      first: [1, 2, 3],
+      second: [3,4],
+      page: 64,
+    },
+    {
+      name: "Conscience environnementale",
+      first: [1, 2, 3],
+      second: [5],
+      page: 39,
     },
     {
       name: "Perception extrasensorielle",
       first: [1, 2, 3],
-      second: [3],
-    },
-    {
-      name: "Super-sens",
-      first: [1, 2, 3],
-      second: [4, 5, 6],
-    },
-    {
-      name: "Sens du danger",
-      first: [4, 5, 6],
-      second: [1, 2],
+      second: [6],
+      page: 94,
     },
     {
       name: "Interface",
       first: [4, 5, 6],
-      second: [3],
+      second: [1],
+      page: 82,
     },
     {
       name: "Post-cognition",
       first: [4, 5, 6],
-      second: [4],
+      second: [2],
+      page: 95,
     },
     {
       name: "Précognition",
       first: [4, 5, 6],
-      second: [5, 6],
+      second: [3],
+      page: 96,
+    },
+    {
+      name: "Super-sens (groupe)",
+      first: [4, 5, 6],
+      second: [4, 5, 6],
+      page: 103,
     },
   ],
   origin: [
@@ -480,7 +529,6 @@ let table = {
     },
     ,
   ],
-
   specialities: [
     [
       "Combat aérien",
