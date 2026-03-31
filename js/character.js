798d87878afaef5df947d92d27e56937c9882389
dxmprofuse <administrateur@dxmprofuse.fr>
2026-03-31 14:01:48 +0200

    Enhance character details in index.html by adding a description section and guidance for selecting aspects. Update character.js to include default values for description and tenacity calculations, improving character creation experience.

--- js/character.js
+++ js/character.js
@@ -8,6 +8,8 @@ class Character {
     attributes = [];
     origin = "";
     specialities = [];
+    description = "   Après avoir déterminé les capacités de votre héros, inventez-lui une histoire et une description. En particulier, imaginez son lieu de naissance, son enfance et son milieu culturel. Comment a-t-il acquis ses pouvoirs surhumains ? Quels sont les éléments de son histoire qui sont à l’origine des motivations et des gageures de sa vie actuelle ? À quoi ressemble votre héros ? Imaginez son physique, son costume, sa taille, sa coiffure et son style, ses manières et tous les autres détails remarquables. Si vous vous sentez l’âme d’un artiste, vous pouvez dessiner votre héros pour aider les autres joueurs à mieux se le représenter. Vous pouvez aussi utiliser ou modifier une image existante pour lui donner son apparence.";
+    tenacite="<div>Soustrayez le nombre total de pouvoirs de votre héros de 6 pour connaître son niveau de Ténacité, avec un minimum de 1 – en gardant à l’esprit que chaque attribut au-dessus de 6 compte comme un pouvoir et que les extras comme les limites peuvent modifier le total de Ténacité que coûte un pouvoir. Le héros commence avec un nombre de points de Ténacité égal à son niveau de Ténacité et ce capital de points est rafraîchi au début de chaque nouveau numéro (mais si le joueur a acquis des points surnuméraires, ceux-ci ne sont pas perdus). <p><strong>Niveau de Ténacité = 6 – nombre total de Pouvoirs</strong></p></div>"
 
     constructor() {
         this.levelingAttributes();
