212fc0879edb1a34667072dad2b266dd6cc94647
dxmprofuse <administrateur@dxmprofuse.fr>
2026-03-31 13:38:47 +0200

    Enhance character management in index.html by adding new action buttons for generating, saving, loading, and clearing characters. Update layout with flexbox for better responsiveness. Improve Vue.js template structure for displaying character attributes and powers, ensuring a more organized presentation.

--- js/data/limites.js
+++ js/data/limites.js
@@ -1,5 +1,5 @@
 let limites = {
-  description:`Une limite rétrécit les effets de base
+  description: `<div>Une limite rétrécit les effets de base
       d’un pouvoir, en échange d’un niveau
       augmenté, d’un impact moindre sur
       la Ténacité ou de l’obtention d’extras
@@ -21,99 +21,117 @@ let limites = {
       +2, pour un maximum de 10.
       </li>
       </ul>
-      +2, pour un maximum de 10.`,
-  list:[
-  {
-    name: "Animaux seulement",
-    value: `Votre pouvoir n’affecte que les animaux.`,
-  },
-  {
-    name: "Au Max",
-    value: `Vous ne pouvez utiliser votre pouvoir qu’au plus fort de sa puissance et vous ne pouvez ni faire varier son niveau, ni le contrôler finement.`,
-  },
-  {
-    name: "Aucune prouesse",
-    value: `Vous ne pouvez réaliser aucune prouesse avec ce pouvoir. Vous ne pourrez donc utiliser que son effet de base.`,
-  },
-  {
-    name: "Bloqué par X",
-    value: `Votre pouvoir n’affecte pas certains objets ou substances communes, comme le bois, l’aluminium ou les choses de couleur bleue, par exemple.`,
-  },
-  {
-    name: "Burnout",
-    value: `Après avoir utilisé votre pouvoir, lancez un dé : sur un résultat de 1 ou 2, votre pouvoir cesse de fonctionner pour le reste du chapitre.`,
-  },
-  {
-    name: "Concentration",
-    value: `Votre pouvoir nécessite de la concentration pour être maintenu (voir Concentration sous Durée). Cette limite ne peut être appliquée qu’à des pouvoirs permanents,
+      </div>
+      <div> <strong>La règle d’or des limites</strong>
+        <p>
+        Une « limite » qui ne limite rien n’en
+        est pas vraiment une ! Les Meneurs
+        de Jeu devraient donc considérer
+        avec prudence les limites qui leur
+        seront proposées et s’assurer qu’elles
+        restreignent réellement la portée ou
+        l’effet d’un pouvoir. Les limites liées
+        à des situations très particulières ou
+        qui ne se produisent que rarement
+        seront mieux gérées comme des
+        complications, offrant aux joueurs
+        des Points de Ténacité quand elles
+        se produisent.
+        </p>
+        </div>
+`,
+  list: [
+    {
+      name: "Animaux seulement",
+      value: `Votre pouvoir n’affecte que les animaux.`,
+    },
+    {
+      name: "Au Max",
+      value: `Vous ne pouvez utiliser votre pouvoir qu’au plus fort de sa puissance et vous ne pouvez ni faire varier son niveau, ni le contrôler finement.`,
+    },
+    {
+      name: "Aucune prouesse",
+      value: `Vous ne pouvez réaliser aucune prouesse avec ce pouvoir. Vous ne pourrez donc utiliser que son effet de base.`,
+    },
+    {
+      name: "Bloqué par X",
+      value: `Votre pouvoir n’affecte pas certains objets ou substances communes, comme le bois, l’aluminium ou les choses de couleur bleue, par exemple.`,
+    },
+    {
+      name: "Burnout",
+      value: `Après avoir utilisé votre pouvoir, lancez un dé : sur un résultat de 1 ou 2, votre pouvoir cesse de fonctionner pour le reste du chapitre.`,
+    },
+    {
+      name: "Concentration",
+      value: `Votre pouvoir nécessite de la concentration pour être maintenu (voir Concentration sous Durée). Cette limite ne peut être appliquée qu’à des pouvoirs permanents,
     continus ou à durée de niveau qui ne requièrent pas déjà de se concentrer.`,
-  },
-  {
-    name: "Constant",
-    value: `Votre pouvoir est toujours actif. Si votre pouvoir cause des dégâts, cela signifie que vous pouvez potentiellement nuire aux gens et aux objets qui vous entourent. 
+    },
+    {
+      name: "Constant",
+      value: `Votre pouvoir est toujours actif. Si votre pouvoir cause des dégâts, cela signifie que vous pouvez potentiellement nuire aux gens et aux objets qui vous entourent. 
     D’autres pouvoirs peuvent vous donner un aspect monstrueux ou simplement vous rendre la vie difficile.`,
-  },
-  {
-    name: "Dégradation",
-    value: `Votre pouvoir perd un niveau après chaque utilisation qui en est faite. Il recouvre les niveaux perdus entre chaque chapitre.`,
-  },
-  {
-    name: "Exclusif",
-    value: `Votre pouvoir est exclusif. Vous ne pouvez utiliser ou maintenir aucun autre pouvoir pendant son utilisation (voir Exclusivité plus haut).`,
-  },
-  {
-    name: "Extra seulement",
-    value: `Vous ne pouvez accomplir qu’un extra de votre pouvoir et pas son effet de base. Vous ne pouvez acquérir aucun autre extra de ce pouvoir ou les utiliser sous forme de prouesse.`,
-  },
-  {
-    name: "Fatigant",
-    value: `Utiliser votre pouvoir vous coûte 2 points d’Endurance.`,
-  },
-  {
-    name: "Imprévisible",
-    value: `Lorsque vous tentez d’utiliser votre pouvoir, lancez un dé. Sur un résultat de 1 ou 2, votre pouvoir ne fonctionne tout simplement pas.`,
-  },
-  {
-    name: "Incontrôlé",
-    value: `Parfois, votre pouvoir s’active sans que vous l’ayez souhaité, sous le contrôle du MJ. Ceci est une limite, pas une complication (et vous ne gagnez donc pas de PT).`,
-  },
-  {
-    name: "Instable",
-    value: `Si vous utilisez votre pouvoir lors d’un combat ou de toute autre situation tendue, lancez un dé. Sur un résultat de 1 ou 2, votre pouvoir fonctionne d’une manière différente de ce que vous souhaitiez.`,
-  },
-  {
-    name: "Ligne de vue",
-    value: `Vous devez être en mesure de voir les cibles de votre pouvoir (ou ils doivent être en mesure de vous voir, à votre choix).`,
-  },
-  {
-    name: "Performance",
-    value: `Vous devez réaliser une performance quelconque pour utiliser votre pouvoir, qu’il s’agisse d’une ritournelle chorégraphiée ou de gestes mystiques accompagnés de formules magiques.
+    },
+    {
+      name: "Dégradation",
+      value: `Votre pouvoir perd un niveau après chaque utilisation qui en est faite. Il recouvre les niveaux perdus entre chaque chapitre.`,
+    },
+    {
+      name: "Exclusif",
+      value: `Votre pouvoir est exclusif. Vous ne pouvez utiliser ou maintenir aucun autre pouvoir pendant son utilisation (voir Exclusivité plus haut).`,
+    },
+    {
+      name: "Extra seulement",
+      value: `Vous ne pouvez accomplir qu’un extra de votre pouvoir et pas son effet de base. Vous ne pouvez acquérir aucun autre extra de ce pouvoir ou les utiliser sous forme de prouesse.`,
+    },
+    {
+      name: "Fatigant",
+      value: `Utiliser votre pouvoir vous coûte 2 points d’Endurance.`,
+    },
+    {
+      name: "Imprévisible",
+      value: `Lorsque vous tentez d’utiliser votre pouvoir, lancez un dé. Sur un résultat de 1 ou 2, votre pouvoir ne fonctionne tout simplement pas.`,
+    },
+    {
+      name: "Incontrôlé",
+      value: `Parfois, votre pouvoir s’active sans que vous l’ayez souhaité, sous le contrôle du MJ. Ceci est une limite, pas une complication (et vous ne gagnez donc pas de PT).`,
+    },
+    {
+      name: "Instable",
+      value: `Si vous utilisez votre pouvoir lors d’un combat ou de toute autre situation tendue, lancez un dé. Sur un résultat de 1 ou 2, votre pouvoir fonctionne d’une manière différente de ce que vous souhaitiez.`,
+    },
+    {
+      name: "Ligne de vue",
+      value: `Vous devez être en mesure de voir les cibles de votre pouvoir (ou ils doivent être en mesure de vous voir, à votre choix).`,
+    },
+    {
+      name: "Performance",
+      value: `Vous devez réaliser une performance quelconque pour utiliser votre pouvoir, qu’il s’agisse d’une ritournelle chorégraphiée ou de gestes mystiques accompagnés de formules magiques.
     Si vous êtes incapables de réaliser la performance, vous ne pouvez pas utiliser votre pouvoir. La réalisation de la performance ne demande pas d’autre action que celle utilisée pour activer le pouvoir,
     sauf si celui-ci nécessite également une Préparation.`,
-  },
-  {
-    name: "Portée proche",
-    value: `Votre pouvoir ne fonctionne qu’à portée proche, nécessitant donc que vous touchiez votre cible.`,
-  },
-  {
-    name: "Préparation",
-    value: `Vous devez vous préparer – et donc ne réaliser aucune autre action – pendant au moins une planche pour activer votre pouvoir.`,
-  },
-  {
-    name: "Seulement sur les autres",
-    value: `Ce pouvoir n’affecte que les autres personnes, vous ne pouvez pas l’utiliser sur vous-même. Cette limite annule le coût de l’extra « Affecte les autres » si vous décidez que seuls les autres peuvent être affectés.`,
-  },
-  {
-    name: "Seulement X",
-    value: `Votre pouvoir ne fonctionne que dans une situation donnée ou contre un sujet bien particulier : seulement la nuit, seulement lorsque vous êtes en colère, seulement contre les hommes ou les femmes et ainsi de suite.`,
-  },
-  {
-    name: "Source",
-    value: `Vous ne pouvez pas générer l’énergie ou la matière utilisée par votre pouvoir et dépendez donc d’une source externe. Le MJ peut limiter le niveau de votre pouvoir en fonction de la disponibilité effective de la source.`,
-  },
-  {
-    name: "Temporaire",
-    value: `Les effets de votre pouvoir ne durent que pendant un nombre de planches égal à son niveau.`,
-  },
-]
+    },
+    {
+      name: "Portée proche",
+      value: `Votre pouvoir ne fonctionne qu’à portée proche, nécessitant donc que vous touchiez votre cible.`,
+    },
+    {
+      name: "Préparation",
+      value: `Vous devez vous préparer – et donc ne réaliser aucune autre action – pendant au moins une planche pour activer votre pouvoir.`,
+    },
+    {
+      name: "Seulement sur les autres",
+      value: `Ce pouvoir n’affecte que les autres personnes, vous ne pouvez pas l’utiliser sur vous-même. Cette limite annule le coût de l’extra « Affecte les autres » si vous décidez que seuls les autres peuvent être affectés.`,
+    },
+    {
+      name: "Seulement X",
+      value: `Votre pouvoir ne fonctionne que dans une situation donnée ou contre un sujet bien particulier : seulement la nuit, seulement lorsque vous êtes en colère, seulement contre les hommes ou les femmes et ainsi de suite.`,
+    },
+    {
+      name: "Source",
+      value: `Vous ne pouvez pas générer l’énergie ou la matière utilisée par votre pouvoir et dépendez donc d’une source externe. Le MJ peut limiter le niveau de votre pouvoir en fonction de la disponibilité effective de la source.`,
+    },
+    {
+      name: "Temporaire",
+      value: `Les effets de votre pouvoir ne durent que pendant un nombre de planches égal à son niveau.`,
+    },
+  ]
 };
